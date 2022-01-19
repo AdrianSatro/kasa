4ccc2a76cc5bcd449e56d2f3495ace103243eee1
AdrianSatro <adrian.satroq@gmail.com>
2022-01-19 18:47:14 +0100

    kasa v1.3
    funkcja zapłaty prawdopodobnie skończona

--- kasav1.cpp
+++ kasav1.cpp
@@ -4,16 +4,21 @@ using namespace std;
 
 void UI();
 void stanKasy();
-void zaplata();
-void reszta();
+float zaplata();
+//void reszta(float x);
 
-float pieniadze[2][15] = { {500, 200, 100 , 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01},
+float Pieniadze[2][15] = { {500, 200, 100 , 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01},
                         {1, 3, 6, 15, 21, 16, 7, 5, 16, 8, 10, 10, 8, 7, 15}};
 
-string templatka[15] = {"500zl","200zl","100zl","50zl","20zl","10zl","5zl","2zl","1zl","50gr","20gr","10gr","5gr","2gr","1gr"};
+string Templatka[15] = {"500zl","200zl","100zl","50zl","20zl","10zl","5zl","2zl","1zl","50gr","20gr","10gr","5gr","2gr","1gr"};
+int IloscNominalu[15]= {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 int main() //main wywołujący funkcje
 { 
+    stanKasy();
+    zaplata();
+   // float SumaDoZaplaty = zaplata();
+    //reszta(SumaDoZaplaty);
     stanKasy();
 
 }
@@ -34,16 +39,55 @@ void stanKasy()
     {
         if (i <= 8) 
         {
-            //templatka[i] = pieniadze[0][i] + "zl";
-            cout << pieniadze[0][i] << "zl: " << pieniadze[1][i] << endl;
+            //Templatka[i] = Pieniadze[0][i] + "zl";
+            cout << Pieniadze[0][i] << "zl: " << Pieniadze[1][i] << endl;
         }
         else
-        cout<<pieniadze[0][i]*100<<"gr: "<<pieniadze[1][i]<<endl;
+        cout<<Pieniadze[0][i]*100<<"gr: "<<Pieniadze[1][i]<<endl;
     }
 }
 
-void zaplata()
+float zaplata()
 {
- cout<<"Ile do zapłaty?";
+    float SumaDoZaplaty=0;
+    string OdKlienta;
+    int Flaga;
+    cout<<"Podaj osobno nominaly ktore wprowadzasz (np 50zl, 20gr)"<<endl;
+
+    while(true)
+    {       
+        int Ilosc = 0; //
+        Flaga = 0;
+        cout<<endl<<"Podaj nominal ktory dajesz (jezeli skonczyles wydawac pieniadze wpisz 'n': ";   
+        cin>>OdKlienta;
+
+        for(int i = 0; i<15; i++) 
+        {
+            if(OdKlienta == Templatka[i])    //sprawdza czy wprowadzona wartosc jest w tabeli z nominalami polskimi
+            {
+
+                cout<<endl<<"Ile nominalow?: ";
+                cin>>Ilosc;
+                IloscNominalu[i]+=Ilosc;
+                //cout<<" "<<Templatka[i]<<" "<<OdKlienta;
+                SumaDoZaplaty+=Pieniadze[0][i]*IloscNominalu[i];
+                Pieniadze[1][i]+=IloscNominalu[i]; 
+                Flaga = 0;
+                break;
+            }
+
+            else if(OdKlienta == "n")
+            {
+                Flaga = 1; 
+                break;
+            }
+            else Flaga = 2;
+        }
+
+        if(Flaga == 1)  break;
+        else if(Flaga == 2) cout<<endl<<"Bledne wprowadzenie";    
+        else continue;
+    }
+    return SumaDoZaplaty;
 }
 
